dccf649fdfb70eacda5936d31cfc987cb1f5bc1d
Vishal Zambare <vishalzambare5@gmail.com>
2021-04-11 13:44:25 +0530

    Create vairables.go

---
+++ Packages, variables, and functions/variables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// bool c, python, java;
+// OR
+// bool python;
+// bool java;
+// bool c;
+var c, python, java bool
+
+func main() {
+	var i int
+	fmt.Println(i, c, python, java)
+}
