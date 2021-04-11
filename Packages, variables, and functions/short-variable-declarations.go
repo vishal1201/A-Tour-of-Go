b9c7055d605ffb1908453ad812d0eeae4228be71
Vishal Zambare <vishalzambare5@gmail.com>
2021-04-11 13:52:21 +0530

    Create short-variable-declarations.go

---
+++ Packages, variables, and functions/short-variable-declarations.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+
+		A variable having a person's name.
+		first_name, middle_name, last_name := "Simran", "Sharos", "Mahadik"
+
+	*/
+	var i, j int = 1, 2
+	k := 3
+	c, python, java := true, false, "no!"
+
+	fmt.Println(i, j, k, c, python, java)
+}
