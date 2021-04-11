ae94fde3fef979c302f1c870b394e2c7ab638cde
Vishal Zambare <vishalzambare5@gmail.com>
2021-04-11 12:00:37 +0530

    Updated hello.go with helpful comments

--- Using the tour/Welcome!/hello.go
+++ Using the tour/Welcome!/hello.go
@@ -1,6 +1,41 @@
+// package section
 package main
 
+// imports
 import "fmt"
+
+// main function or other source code
+/*
+Java --
+	public static void main(String[] args) {}
+Java --
+
+python --
+if __name__ == '__main__':
+	pass
+python --
+
+C --
+
+#include <stdio.h>
+#include <conio.h> //NOT NEEDED!!
+
+void main() {
+	//WRONG!!
+}
+
+int main(int argc, char* argv[]) {
+	return 0;
+}
+
+C --
+
+Go --
+
+func main() {}
+
+Go --
+*/
 
 func main() {
 	fmt.Println("Hello, Penguin")
