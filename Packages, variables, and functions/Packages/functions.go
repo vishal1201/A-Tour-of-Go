3f547386275f3d39d6629b82f159326c8abf75a5
Vishal Zambare <vishalzambare5@gmail.com>
2021-04-11 13:24:04 +0530

    Create functions.go

---
+++ Packages, variables, and functions/Packages/functions.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+/*
+characteristics - has a name
+	- Input - Arguments
+	- Processing - Function logic/purpose
+	- Output - return values
+*/
+//  name  arg1, arg2    return-type
+/*
+
+int add(int x, int y) {
+	return x + y
+}
+
+*/
+func add(x int, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println(add(42, 13))
+}
