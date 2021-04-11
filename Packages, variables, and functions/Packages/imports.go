a98531cc7e04da33fe246803f5be318a88df7d9f
Vishal Zambare <vishalzambare5@gmail.com>
2021-04-11 12:38:31 +0530

    Create imports.go

---
+++ Packages, variables, and functions/Packages/imports.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	// Formatted Print. i.e. will help you print valus 'in place'
+	// with the help of verbs.
+	fmt.Printf("Now you have %g problems of type.\n", math.Sqrt(7))
+}
